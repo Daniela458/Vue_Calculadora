8b70cfc0c4a600e94b687b5663f682226108ec58
Daniela458 <dan.da.d.458@gmail.com>
2022-04-29 10:07:47 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-container>
+    <div class="mt-4">
+      <b-row>
+      <b-col cols="4">
+    <b-form-input v-model="num1" placeholder="numero 1"></b-form-input>
+    </b-col>
+     <b-col cols="4">
+    <div>
+    <b-form-select
+      v-model="selected"
+      :options="options"
+      class="mb-3"
+      value-field="item"
+      text-field="name"
+      disabled-field="notEnabled"
+    ></b-form-select>
   </div>
+    
+    </b-col>
+    <b-col cols='4'>
+    <b-form-input v-model="num2" placeholder="numero 2"></b-form-input>
+    </b-col>
+    </b-row>
+  </div>
+
+    <b-button variant="outline-info" @click="calculo()" class="mt-4">calcular</b-button>
+  
+  {{resultado}}
+  </b-container>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data() {
+      return {
+        num1: 0,
+        num2:0,
+        selected: '',
+        options: [
+          { item: 'A', name: '+' },
+          { item: 'B', name: '-' },
+          { item: 'C', name: '*' },
+          { item: 'D', name: '/' }
+        ],
+        resultado:0
+      }},
   components: {
-    HelloWorld
-  }
+    
+  },
+  
+  methods:{
+    calculo: function(){
+
+      switch(this.selected){
+        case 'A':
+          this.resultado =  parseInt(this.num1) + parseInt(this.num2)
+          break
+        case 'B':
+          this.resultado = this.num1 - this.num2
+          break
+        case 'C':
+          this.resultado = this.num1 * this.num2
+          break
+        case 'D':
+          this.resultado = this.num1 / this.num2
+        //console.log(this.resultado)
+      }
+    }
 }
+}
+
 </script>
 
 <style>
